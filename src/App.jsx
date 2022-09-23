d1352afd9bf6a566867e0480d7ebeebe15dee439
daridev <hernandezdarifrancisco@gmail.com>
2022-09-23 09:09:58 -0500

    custom cards 2

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,63 @@ import Card from './components/Card'
 function App() {
     const [count, setCount] = useState(0)
 
+    // Sample text
+    const text =
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti repellat dolor dolorum quisquam nisi magni nulla sint ea quasi aliquid accusamus, architecto dicta.'
+
     return (
         <main className='container w-100'>
             <div className='row'>
-                <div className='col-12 col-lg-9 bg-primary'>
+                {/* Remove bg-color in wrapper */}
+                <div className='col-12 col-lg-9'>
                     <div className='row'>
-                        <div className='col-12 col-lg-8 bg-dark'>
-                            {/* Card component */}
-                            <Card image="daniel" name="daniel clifford" />
+                        {/* Custom padding in rows */}
+                        <div className='col-12 col-lg-8 p-2'>
+                            {/* Submit bgcolor class and custom text to cards  */}
+                            <Card
+                                image='daniel'
+                                name='daniel clifford'
+                                bgcolor='bg-primary'
+                                texts={[text, text]}
+                            />
                         </div>
 
-                        <div className='col-12 col-lg-4 bg-success'>
-                            <Card image="jonathan" name="jonathan walters" />
+                        <div className='col-12 col-lg-4 p-2'>
+                            <Card
+                                image='jonathan'
+                                name='jonathan walters'
+                                bgcolor='bg-success'
+                                texts={[text]}
+                            />
                         </div>
 
-                        <div className='col-12 col-lg-4 bg-warningt'>
-                            <Card image="jeanette" name="jeanette harmon" />
+                        <div className='col-12 col-lg-4 p-2'>
+                            <Card
+                                image='jeanette'
+                                name='jeanette harmon'
+                                bgcolor='bg-warning'
+                                texts={[text]}
+                            />
                         </div>
 
-                        <div className='col-12 col-lg-8 bg-info'>
-                            <Card image="patrick" name="patrick abrams" />
+                        <div className='col-12 col-lg-8 p-2'>
+                            <Card
+                                image='patrick'
+                                name='patrick abrams'
+                                bgcolor='bg-info'
+                                texts={[text, text]}
+                            />
                         </div>
                     </div>
                 </div>
 
-                <div className='col-12 col-lg-3 bg-secondary'>
-                    <Card image="kira" name="kira whittle" />
+                <div className='col-12 col-lg-3 p-2'>
+                    <Card
+                        image='kira'
+                        name='kira whittle'
+                        bgcolor='bg-secondary'
+                        texts={[text, text, text]}
+                    />
                 </div>
             </div>
         </main>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,45 +1,43 @@
 import PropTypes from 'prop-types'
 
-function Card({ image, name }) {
+function Card({ image, name, bgcolor, texts }) {
     return (
-        <article className='card text-start p-0'>
+        // Add bgcolor class to card
+        <article className={`card text-start p-2 h-100 ${bgcolor}`}>
             <div className='card-body'>
                 <div className='d-flex align-items-center mb-3'>
                     <img
-                        // props image
                         src={`imgs/image-${image}.jpg`}
                         alt=''
                         className='border border-secondary border-3 rounded-circle w-1'
                     />
 
                     <div className='ps-3'>
-                        { /* props name  */}
                         <h2 className='m-0 h6 text-capitalize'>{name}</h2>
                         <p className='m-0'>Lorem Lorem</p>
                     </div>
                 </div>
 
-                <p className='fs-6 lh-1'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Voluptas aut quasi natus eligendi, deleniti ut magni
-                    repudiandae asperiores, non quae est odit blanditiis, qui
-                    voluptate. Provident iusto at ducimus quas!
-                </p>
-                <p className='fs-6 lh-1'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea
-                    eos nulla asperiores illo sed facere architecto iusto sit
-                    esse incidunt perferendis odio porro aut corrupti laborum
-                    fugiat quo, tempore dolorem.
-                </p>
+                {/* Loop for each text element */}
+                {texts.map((text, text_id) => (
+                    // Generate paragraphs
+                    <p
+                        className='fs-6 lh-1'
+                        key={text_id}
+                    >
+                        {text}
+                    </p>
+                ))}
             </div>
         </article>
     )
 }
 
-// Props datatype
 Card.propTypes = {
     image: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
+    bgcolor: PropTypes.string.isRequired,
+    texts: PropTypes.arrayOf(PropTypes.string).isRequired,
 }
 
 export default Card
